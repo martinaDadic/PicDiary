26f29d28bd0580d5aa1b66e1159b3ccc94b21f99
Martina Dadić <md54715@fer.hr>
2026-02-02 02:19:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const video = document.querySelector("#camera");
+async function StartStream(){
+    if (navigator.mediaDevices.getUserMedia) {
+        try{
+            stream = await navigator.mediaDevices.getUserMedia({video: true});
+            video.srcObject=stream;
+            video.play();
+        }catch(err){
+            alert("molim vas dopustite pristup kameri")
+        }
+    }else {
+        alert("Tvoj browser ne podržava pristup kameri.");
+    }
+}
+
+StartStream();
+
+const button = document.getElementById("snapButton");
+const canvas = document.getElementById("photo");
+const img = document.getElementById("result");
+
+button.addEventListener("click", () => {
+  const context = canvas.getContext("2d");
+  canvas.width = 300;
+  canvas.height = 200;
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  const imageData = canvas.toDataURL("image/png");
+  img.src = imageData;
+});
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/sw.js")
+    });
+}
---
+++ sw.js
@@ -0,0 +1,34 @@
+const filesToCache= [
+    "/",
+    "manifest.json",
+    "index.html",
+    "404.html",
+    "offline.html"
+];
+const staticCacheName= "static-cache-v1";
+self.addEventListener("install", (event) => {
+    console.log("Attempting to install service worker and cache static assets");
+    event.waitUntil(caches.open(staticCacheName).then((cache) =>{
+        return cache.addAll(filesToCache);
+    }));
+});
+
+self.addEventListener("fetch", (event) => {event.respondWith(
+    caches.match(event.request).then((response) => {
+        if (response) {
+            return response;
+        }
+        return fetch(event.request).then((response) =>{
+            if (response.status === 404) {
+                return caches.match("404.html");
+            }
+            return caches.open(staticCacheName).then((cache) =>{
+                cache.put(event.request.url, response.clone());
+                return response;
+            });
+        });
+    }).catch((error) =>{
+        return caches.match("offline.html");
+    })
+  );
+});
